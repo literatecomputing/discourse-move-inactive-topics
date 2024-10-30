414abd9c481e72cf23f7b93319ab879cba23adb4
Jay Pfaffman <jay@literatecomputing.com>
2024-10-30 10:08:17 -0500

    remove route skeleton

--- config/routes.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-DiscourseMoveInactiveTopics::Engine.routes.draw do
-  get "/examples" => "examples#index"
-  # define routes here
-end
-
-Discourse::Application.routes.draw { mount ::DiscourseMoveInactiveTopics::Engine, at: "discourse-move-inactive-topics" }
